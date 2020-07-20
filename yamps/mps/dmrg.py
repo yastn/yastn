c63601696ddb19f60d4410b0aa654b39441f9bf2
Uniwersytet Jagiellonski - Fizyka 1 <fizyka@iMac-Uniwersytet.local>
2020-07-20 17:53:57 +0200

    dmrg-0site and 1site and 2site and 2site_group

--- yamps/mps/dmrg.py
+++ yamps/mps/dmrg.py
@@ -4,16 +4,12 @@ from yamps.tensor import eigs
 #################################
 #           dmrg                #
 #################################
-
-def dmrg_sweep_1site(psi, H, env=None, dtype='complex128', hermitian=True, k=4, eigs_tol=1e-14, opts_svd=None):
+def dmrg_sweep_0site(psi, H, env=None, dtype='complex128', hermitian=True, k=4, eigs_tol=1e-14, opts_svd=None):
     """ 
-    Perform sweep of single-site DMRG. 
+    Perform sweep with 0site-DMRG where update is made on the central site.
     Assume input psi is right canonical. 
     Sweep consists of iterative updates from last site to first and back to the first one. 
     
-    Output:
-    env - overlap <psi| H |psi> as Env3.
-    psi is self updated.
     
     Parameters
     ----------
@@ -49,6 +45,111 @@ def dmrg_sweep_1site(psi, H, env=None, dtype='complex128', hermitian=True, k=4, 
     opts_svd: dict
         default=None
         options for truncation
+
+    Returns
+    -------
+    env: Env3
+     Overlap <psi| H |psi> as Env3.
+    
+    psi: Mps
+        Is self updated.
+    """
+
+    if env is None:
+        env = Env3(bra=psi, op=H, ket=psi)
+        env.setup_to_first()
+
+    for n in psi.g.sweep(to='last'): # sweep from fist to last 
+        psi.orthogonalize_site(n, towards=psi.g.last)
+        env.clear_site(n)
+        env.update(n, towards=psi.g.last)
+        if n!=psi.g.sweep(to='last')[-1]:
+            init = psi.A[psi.pC]
+            # update site n using eigs
+            if not hermitian:
+                val, vec, _ = eigs(Av=lambda v: env.Heff0(v, psi.pC), Bv=lambda v: env.Heff0(v, psi.pC, conj=True), init=[init], tol=eigs_tol, k=k, hermitian=True, dtype=dtype)
+            else:
+                val, vec, _ = eigs(Av=lambda v: env.Heff0(v, psi.pC), init=[init], tol=eigs_tol, k=k, hermitian=True, dtype=dtype)
+            init = vec[list(val).index(min(list(val)))]
+            # canonize and save
+            if opts_svd != None:
+                U, S, V = init.split_svd(axes=((0), (1)), sU=-1, **opts_svd)
+                psi.A[psi.pC] = U.dot(S.dot(V, axes=((1),(0))) , axes=((1),(0)))
+            else:
+                psi.A[psi.pC] = init
+        psi.absorb_central(towards=psi.g.last)
+
+    for n in psi.g.sweep(to='first'):
+        psi.orthogonalize_site(n, towards=psi.g.first)
+        env.clear_site(n)
+        env.update(n, towards=psi.g.first)
+        if n!=psi.g.sweep(to='first')[-1]:
+            init = psi.A[psi.pC]
+            # update site n using eigs
+            if not hermitian:
+                val, vec, _ = eigs(Av=lambda v: env.Heff0(v, psi.pC), Bv=lambda v: env.Heff0(v, psi.pC, conj=True), init=[init], tol=eigs_tol, k=k, hermitian=True, dtype=dtype)
+            else:
+                val, vec, _ = eigs(Av=lambda v: env.Heff0(v, psi.pC), init=[init], tol=eigs_tol, k=k, hermitian=True, dtype=dtype)
+            init = vec[list(val).index(min(list(val)))]
+            # canonize and save
+            if opts_svd != None:
+                U, S, V = init.split_svd(axes=((0), (1)), sU=-1, **opts_svd)
+                psi.A[psi.pC] = U.dot(S.dot(V, axes=((1),(0))) , axes=((1),(0)))
+            else:
+                psi.A[psi.pC] = init
+        psi.absorb_central(towards=psi.g.first)
+
+    return env
+
+
+def dmrg_sweep_1site(psi, H, env=None, dtype='complex128', hermitian=True, k=4, eigs_tol=1e-14, opts_svd=None):
+    """ 
+    Perform sweep of single-site DMRG. 
+    Assume input psi is right canonical. 
+    Sweep consists of iterative updates from last site to first and back to the first one. 
+    
+    Parameters
+    ----------
+    psi: Mps, nr_phys=1, nr_aux=0 or 1
+        Initial state.
+        Can be gives as an MPS (nr_aux=0) or a purification (nr_aux=1).
+
+    H: Mps, nr_phys=2
+        Operator given in MPO decomposition. 
+        Legs are [left-virtual, ket-physical, bra-physical, right-virtual]
+
+    env: Env3
+        default = None
+        Initial overlap <psi| H |psi>
+        Initial environments must be set up with respect to the last site.
+        
+    dtype: str
+        default='complex128'
+        Type of Tensor.
+
+    hermitian: bool
+        default=True
+        Is MPO hermitian
+            
+    k: int 
+        default=4
+        Dimension of Krylov subspace for eigs(.)
+            
+    eigs_tol: float
+        default=1e-14
+        Cutoff for krylov subspace for eigs(.)
+            
+    opts_svd: dict
+        default=None
+        options for truncation
+    
+    Returns
+    -------
+    env: Env3
+     Overlap <psi| H |psi> as Env3.
+    
+    psi: Mps
+        Is self updated.
     """
 
     if env is None:
@@ -106,10 +207,6 @@ def dmrg_sweep_2site(psi, H, env=None, dtype='complex128', hermitian=True, k=4, 
     Assume input psi is right canonical. 
     Sweep consists of iterative updates from last site to first and back to the first one. 
     
-    Output:
-    env - overlap <psi| H |psi> as Env3.
-    psi is self updated.
-    
     Parameters
     ----------
     psi: Mps, nr_phys=1, nr_aux=0 or 1
@@ -144,6 +241,14 @@ def dmrg_sweep_2site(psi, H, env=None, dtype='complex128', hermitian=True, k=4, 
     opts_svd: dict
         default=None
         options for truncation
+    
+    Returns
+    -------
+    env: Env3
+     Overlap <psi| H |psi> as Env3.
+    
+    psi: Mps
+        Is self updated.
     """
 
     if env is None:
@@ -194,10 +299,6 @@ def dmrg_sweep_2site_group(psi, H, env=None, dtype='complex128', hermitian=True,
     Assume input psi is right canonical. 
     Sweep consists of iterative updates from last site to first and back to the first one. 
     
-    Output:
-    env - overlap <psi| H |psi> as Env3.
-    psi is self updated.
-    
     Parameters
     ----------
     psi: Mps, nr_phys=1, nr_aux=0 or 1
@@ -232,6 +333,14 @@ def dmrg_sweep_2site_group(psi, H, env=None, dtype='complex128', hermitian=True,
     opts_svd: dict
         default=None
         options for truncation
+    
+    Returns
+    -------
+    env: Env3
+     Overlap <psi| H |psi> as Env3.
+    
+    psi: Mps
+        Is self updated.
     """
 
     if env is None:
