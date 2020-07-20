c2e3f925953ac5572d422b7a6857b7535f25a26e
Uniwersytet Jagiellonski - Fizyka 1 <fizyka@iMac-Uniwersytet.local>
2020-07-20 17:05:37 +0200

    dmrg-1site and 2site and 2site_group

--- yamps/mps/dmrg.py
+++ yamps/mps/dmrg.py
@@ -7,7 +7,8 @@ from yamps.tensor import eigs
 
 def dmrg_sweep_1site(psi, H, env=None, dtype='complex128', hermitian=True, k=4, eigs_tol=1e-14, opts_svd=None):
     """ 
-    Perform sweep of single-site DMRG. Assume input psi is right canonical. 
+    Perform sweep of single-site DMRG. 
+    Assume input psi is right canonical. 
     Sweep consists of iterative updates from last site to first and back to the first one. 
     
     Output:
@@ -101,7 +102,8 @@ def dmrg_sweep_1site(psi, H, env=None, dtype='complex128', hermitian=True, k=4, 
 
 def dmrg_sweep_2site(psi, H, env=None, dtype='complex128', hermitian=True, k=4, eigs_tol=1e-14, opts_svd={}):
     """ 
-    Perform sweep of two-site DMRG. Assume input psi is right canonical. 
+    Perform sweep of two-site DMRG. 
+    Assume input psi is right canonical. 
     Sweep consists of iterative updates from last site to first and back to the first one. 
     
     Output:
@@ -168,6 +170,7 @@ def dmrg_sweep_2site(psi, H, env=None, dtype='complex128', hermitian=True, k=4, 
     for n in psi.g.sweep(to='first', dl=1):
         n1, _, _ = psi.g.from_site(n, towards=psi.g.last)
         init = psi.A[n].dot(psi.A[n1], axes=(psi.right, psi.left))
+        # update site n using eigs
         if not hermitian:
             val, vec, _ = eigs(Av=lambda v: env.Heff2(v, n), Bv=lambda v: env.Heff2(v, n, conj=True), init=[init], tol=eigs_tol, k=k, hermitian=True, dtype=dtype)
         else:
@@ -186,9 +189,51 @@ def dmrg_sweep_2site(psi, H, env=None, dtype='complex128', hermitian=True, k=4, 
 
 
 def dmrg_sweep_2site_group(psi, H, env=None, dtype='complex128', hermitian=True, k=4, eigs_tol=1e-14, opts_svd={}):
+    """ 
+    Perform sweep of two-site DMRG with groupping neigbouring sites. 
+    Assume input psi is right canonical. 
+    Sweep consists of iterative updates from last site to first and back to the first one. 
+    
+    Output:
+    env - overlap <psi| H |psi> as Env3.
+    psi is self updated.
+    
+    Parameters
+    ----------
+    psi: Mps, nr_phys=1, nr_aux=0 or 1
+        Initial state.
+        Can be gives as an MPS (nr_aux=0) or a purification (nr_aux=1).
+
+    H: Mps, nr_phys=2
+        Operator given in MPO decomposition. 
+        Legs are [left-virtual, ket-physical, bra-physical, right-virtual]
+
+    env: Env3
+        default = None
+        Initial overlap <psi| H |psi>
+        Initial environments must be set up with respect to the last site.
+        
+    dtype: str
+        default='complex128'
+        Type of Tensor.
+
+    hermitian: bool
+        default=True
+        Is MPO hermitian
+            
+    k: int 
+        default=4
+        Dimension of Krylov subspace for eigs(.)
+            
+    eigs_tol: float
+        default=1e-14
+        Cutoff for krylov subspace for eigs(.)
+            
+    opts_svd: dict
+        default=None
+        options for truncation
     """
-    Assume psi is in the left cannonical form.
-    """
+
     if env is None:
         env = Env3(bra=psi, op=H, ket=psi)
         env.setup_to_first()
@@ -197,11 +242,15 @@ def dmrg_sweep_2site_group(psi, H, env=None, dtype='complex128', hermitian=True,
         n1, _, _ = psi.g.from_site(n, towards=psi.g.last)
         init = psi.A[n].dot(psi.A[n1], axes=(psi.right, psi.left))
         init, leg_order = init.group_legs(axes=(1, 2), new_s=1)
-        val, vec, happy = eigs(Av=lambda v: env.Heff2_group(v, n), init=[init], tol=eigs_tol, k=k, hermitian=True, dtype=dtype)
-        val = list(val)
-        out = vec[val.index(min(val))]
-        out = out.ungroup_leg(axis=1, leg_order=leg_order)
-        A1, S, A2 = out.split_svd(axes=((0, 1), (2, 3)), sU=-1, **opts_svd)
+        # update site n using eigs
+        if not hermitian:
+            val, vec, _ = eigs(Av=lambda v: env.Heff2_group(v, n), Bv=lambda v: env.Heff2_group(v, n, conj=True), init=[init], tol=eigs_tol, k=k, hermitian=True, dtype=dtype)
+        else:
+            val, vec, _ = eigs(Av=lambda v: env.Heff2_group(v, n), init=[init], tol=eigs_tol, k=k, hermitian=True, dtype=dtype)
+        init = vec[list(val).index(min(list(val)))]
+        init = init.ungroup_leg(axis=1, leg_order=leg_order)
+        #split and save
+        A1, S, A2 = init.split_svd(axes=(psi.left + psi.phys + psi.aux, tuple(a + psi.right[0] - 1 for a in psi.phys + psi.aux + psi.right)), sU=-1, **opts_svd)
         psi.A[n] = A1
         psi.A[n1] = A2.dot_diag(S, axis=0)
         env.clear_site(n)
@@ -212,11 +261,15 @@ def dmrg_sweep_2site_group(psi, H, env=None, dtype='complex128', hermitian=True,
         n1, _, _ = psi.g.from_site(n, towards=psi.g.last)
         init = psi.A[n].dot(psi.A[n1], axes=(psi.right, psi.left))
         init, leg_order = init.group_legs(axes=(1, 2), new_s=1)
-        val, vec, happy = eigs(Av=lambda v: env.Heff2_group(v, n), init=[init], tol=eigs_tol, k=k, hermitian=True, dtype=dtype)
-        val = list(val)
-        out = vec[val.index(min(val))]
-        out = out.ungroup_leg(axis=1, leg_order=leg_order)
-        A1, S, A2 = out.split_svd(axes=((0, 1), (2, 3)), sU=-1, **opts_svd)
+        # update site n using eigs
+        if not hermitian:
+            val, vec, _ = eigs(Av=lambda v: env.Heff2_group(v, n), Bv=lambda v: env.Heff2_group(v, n, conj=True), init=[init], tol=eigs_tol, k=k, hermitian=True, dtype=dtype)
+        else:
+            val, vec, _ = eigs(Av=lambda v: env.Heff2_group(v, n), init=[init], tol=eigs_tol, k=k, hermitian=True, dtype=dtype)
+        init = vec[list(val).index(min(list(val)))]
+        init = init.ungroup_leg(axis=1, leg_order=leg_order)
+        #split and save
+        A1, S, A2 = init.split_svd(axes=(psi.left + psi.phys + psi.aux, tuple(a + psi.right[0] - 1 for a in psi.phys + psi.aux + psi.right)), sU=-1, **opts_svd)
         psi.A[n] = A1.dot_diag(S, axis=2)
         psi.A[n1] = A2
         env.clear_site(n)
