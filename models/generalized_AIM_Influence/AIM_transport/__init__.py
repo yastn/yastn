0648bc0a49d2cdb7d30bcfd40efa5b23d234fdf2
g.wojtowicz@doctoral.uj.edu.pl <g.wojtowicz@doctoral.uj.edu.pl>
2020-09-02 12:41:36 +0200

    remove models

--- models/generalized_AIM_Influence/AIM_transport/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import transport_vectorization_full
-from . import transport_vectorization_Z2
-from . import transport_vectorization_U1
--- models/generalized_AIM_Influence/AIM_transport/basis/__init__.py
+++
@@ -1,2 +0,0 @@
-from .influence_functions import influence_LinLin, influence_LinInv, influence_LinInv1D, influence_LinLog, influence_LinLog1D
-from .wdk_AS import wdk_AS
