9b5b2acfa64b152e0b9015245649b7661580cf28
marekrams <marek.rams@uj.edu.pl>
2021-04-06 10:04:07 +0200

    fix import in test

--- tests/yast/run_command.py
+++ tests/yast/run_command.py
@@ -3,8 +3,8 @@ import sys
 sys.path.append(os.path.join(os.path.dirname(__file__), './../..'))  # folder with yast
 sys.path.append(os.path.join(os.path.dirname(__file__), './../configs'))  # folder with configs for tests
 from test_swap_gate import test_swap_1
-from test_commands import test_commands
+from test_syntax import test_sytax
 
 if __name__ == '__main__':
     # test_swap_1()
-    test_commands()
+    test_syntax()
