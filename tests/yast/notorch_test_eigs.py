978a1573b10011f984f3686b7011f0ef5a412aa5
marekrams <marek.rams@uj.edu.pl>
2021-04-26 19:23:01 +0200

    test_eigs example with fused legs

--- tests/yast/notorch_test_eigs.py
+++ tests/yast/notorch_test_eigs.py
@@ -3,7 +3,7 @@ from context import config_U1
 import numpy as np
 from scipy.sparse.linalg import eigs, LinearOperator
 
-tol = 1e-12
+tol = 1e-10
 
 # import yast.backend.backend_torch as backend
 # config_U1_R.backend = backend
@@ -47,7 +47,7 @@ def test_eigs_simple():
 
 
 
-def test_eigs_sparse():
+def test_eigs_exception():
     a = yast.rand(config=config_U1, s=(1, 1, -1), n=0,
                   t=[(-2, -1, 0, 1), (0, 1), (-1, 0, 1, 2)],
                   D=[(1, 2, 3, 4), (2, 3), (2, 3, 4, 5)])
@@ -72,20 +72,36 @@ def test_eigs_sparse():
         t2 = yast.ncon([a, a, t], [(-1, 1, 2), (-2, 1, 3), (2, 3, -3)], conjs=(0, 1, 0))
         t3, _ = yast.compress_to_1d(t2, meta=meta)
         return t3
-
     ff = LinearOperator(shape=(len(r1d), len(r1d)), matvec=f, dtype=np.float64)
 
     # eigs going though yast.tensor
     wy, vy1d = eigs(ff, v0=r1d, k=9, which='LM', tol=1e-10)
     print(wy)  # eigenvalues
 
+
+    # for tm with fused legs
+    vv2 = yast.randR(config=a.config, legs=[(tm, 1, 'f', tm, 0), {'s':1, -1:1, 0:1, 1:1}])
+    r1d2, meta2 = yast.compress_to_1d(vv2)
+
+    def f2(x):
+        t = yast.decompress_from_1d(x, config_U1, meta2)
+        t2 = yast.ncon([tm, t], [(-1, 1), (1, -2)], conjs=(0, 0))
+        t3, _ = yast.compress_to_1d(t2, meta=meta2)
+        return t3
+
+    ff2 = LinearOperator(shape=(len(r1d2), len(r1d2)), matvec=f2, dtype=np.float64)
+
+    wy, vy2d = eigs(ff2, v0=r1d2, k=9, which='LM', tol=1e-10)
+    print(wy)  # eigenvalues
+
+
     # transform eigenvectors into yast tensors
     vy = [yast.decompress_from_1d(x, config_U1, meta) for x in vy1d.T]
     # remove zero blocks
-    vyr = [yast.remove_zero_blocks(a) for a in vy]
+    vyr = [yast.remove_zero_blocks(a, rel=1e-12) for a in vy]
     assert all((yast.norm_diff(x, y) < tol for x, y in zip(vy, vyr)))
     # display charges of eigenvectors (only charge on last leg) -- now there is superposition between +1 and -1
     print([x.get_leg_structure(axis=2) for x in vyr])
 
 if __name__ == '__main__':
-    test_eigs_sparse()
+    test_eigs_exception()
