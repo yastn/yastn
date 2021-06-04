58d6daa85c741253d7fcf1e33f6e853d4e6d6788
Juraj Hasik <juraj.hasik.sk@gmail.com>
2021-06-04 10:59:41 +0200

    parallel omp merge to matrix

--- experimental/torch_mmib.cpp
+++ experimental/torch_mmib.cpp
@@ -1,6 +1,7 @@
 #include <torch/extension.h>
 #include <iostream>
 #include <vector>
+#include <omp.h>
 
 std::tuple< std::vector<std::vector<int>>, std::vector<torch::Tensor> > mmib_forward(
 	std::map< std::vector<int>, torch::Tensor > A,
@@ -9,13 +10,31 @@ std::tuple< std::vector<std::vector<int>>, std::vector<torch::Tensor> > mmib_for
 	){
 	std::vector<std::vector<int>> res_i;
 	std::vector<torch::Tensor> res_t;
+
 	for ( auto const &t : meta_dot ) { 
-		// std::cout << t[0] << "|" << t[1] << "|" << t[2] << std::endl;
 		res_i.push_back(t[0]);
 		res_t.push_back(A[t[1]].mm(B[t[2]]));
 	}
+
 	return std::make_tuple( res_i, res_t );
 }
+
+// std::vector< std::pair<std::vector<int64_t>, torch::Tensor> > mmib_forward(
+// 	std::map< std::vector<int64_t>, torch::Tensor > A,
+// 	std::map< std::vector<int64_t>, torch::Tensor > B,
+// 	std::vector< std::vector< std::vector< int64_t > > > meta_dot
+// 	){
+// 	std::map< std::vector<int64_t>, torch::Tensor > res_map; 
+
+// 	#pragma omp parallel for
+// 	for (auto it = meta_dot.begin(); it < meta_dot.end(); it++) {
+// 		res_map[(*it)[0]]= A[(*it)[1]].mm(B[(*it)[2]]);
+// 	}
+// 	std::vector< std::pair<std::vector<int64_t>, torch::Tensor> > res_vec = 
+// 		std::vector< std::pair<std::vector<int64_t>, torch::Tensor> >(res_map.begin(), res_map.end());
+
+// 	return res_vec;
+// }
 
 void mmib_backward(
 	std::vector<torch::Tensor> A,
