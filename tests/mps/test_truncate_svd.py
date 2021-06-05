4f8a87566a9d31da7c6590d91e1c520f1050c79d
marekrams <marek.rams@uj.edu.pl>
2021-06-05 17:10:58 +0200

    cleaning mps

--- tests/mps/test_truncate_svd.py
+++ tests/mps/test_truncate_svd.py
@@ -7,7 +7,7 @@ import pytest
 
 
 def run_dmrg_1site(psi, H, sweeps=10):
-    """ Run a faw sweeps of dmrg_1site_sweep. Returns energy. """
+    """ Run a few sweeps of dmrg_1site_sweep. Returns energy. """
     env = None
     for _ in range(sweeps):
         env = yamps.dmrg.dmrg_sweep_1site(psi, H, env=env)
@@ -19,9 +19,9 @@ def run_truncation(psi, H, Egs, sweeps=2):
     discarded = psi2.truncate_sweep(to='last', opts={'D_total': 4})
 
     ov_t = yamps.measure_overlap(psi, psi2)
-    Eng_t = yamps.measure_mpo(psi2, H, psi2) 
+    Eng_t = yamps.measure_mpo(psi2, H, psi2)
     assert 1 > ov_t > 0.99
-    assert Egs < Eng_t < Egs*0.99
+    assert Egs < Eng_t < Egs * 0.99
 
     psi2.canonize_sweep(to='first')
     env = None
@@ -29,7 +29,8 @@ def run_truncation(psi, H, Egs, sweeps=2):
         env = yamps.sweep_variational(psi2, psi_target=psi, env=env)
 
     ov_v = yamps.measure_overlap(psi, psi2)
-    Eng_v = yamps.measure_mpo(psi2, H, psi2) 
+    Eng_v = yamps.measure_mpo(psi2, H, psi2)
+    assert psi2.get_bond_dimensions() == [1, 2, 4, 4, 4, 4, 4, 2, 1]
     assert 1 > ov_v > ov_t
     assert Egs < Eng_v < Eng_t
 
@@ -60,11 +61,13 @@ def test_truncate_svd_Z2():
 
     H = ops_Z2.mpo_XX_model(N=N, t=1, mu=0)
 
-    psi = ops_Z2.mps_random(N=N, Dblock=D_total/2, total_parity=1).canonize_sweep(to='first')
+    psi = ops_Z2.mps_random(N=N, Dblock=D_total/2, total_parity=1)
+    psi.canonize_sweep(to='first')
     run_dmrg_1site(psi, H)
     run_truncation(psi, H, Eng_parity1)
 
-    psi = ops_Z2.mps_random(N=N, Dblock=D_total/2, total_parity=0).canonize_sweep(to='first')
+    psi = ops_Z2.mps_random(N=N, Dblock=D_total/2, total_parity=0)
+    psi.canonize_sweep(to='first')
     run_dmrg_1site(psi, H)
     run_truncation(psi, H, Eng_parity0)
 
