4aef50dc087b63ce992c61d6f6df0a4f7d8e4f59
marekrams <marek.rams@uj.edu.pl>
2021-07-05 09:52:49 +0200

    conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -15,8 +15,8 @@ def pytest_configure(config):
         print('Using numpy backend')
         import yast.backend.backend_np as backend
 
-    for folder in ["tensor"]:
-        confs = [name[:-3] for name in os.listdir(folder + "\configs") if fnmatch(name, 'config*.py')]
+    for folder in ["tensor", "mps"]:
+        confs = [name[:-3] for name in os.listdir(folder + "/configs") if fnmatch(name, 'config*.py')]
         for conf in confs:
             conf = importlib.import_module(folder + ".configs." + conf)
             conf.backend = backend
