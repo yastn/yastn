6ef791afb939a958aeecc8f28d9acd24509c7450
g.wojtowicz@doctoral.uj.edu.pl <g.wojtowicz@doctoral.uj.edu.pl>
2021-07-22 13:01:37 +0200

    tests and functions for automatic Mps generator and addition of Mps-s

--- tests/mps/ops_dense.py
+++ tests/mps/ops_dense.py
@@ -94,3 +94,39 @@ def mpo_occupation(N):
         tmp = np.transpose(tmp, (0, 1, 3, 2))
         H.A[n].set_block(val=tmp, Ds=Ds)
     return H
+
+
+def mpo_gen_XX(chain, t, mu):
+    Ds, s = (2, 2), (1, -1)
+
+    CP = yast.Tensor(config=config_dense, s=s)
+    CP.set_block(Ds=Ds, val=[[0, 0], [1, 0]])
+    C = yast.Tensor(config=config_dense, s=s)
+    C.set_block(Ds=Ds, val=[[0, 1], [0, 0]])
+    NN = yast.Tensor(config=config_dense, s=s)
+    NN.set_block(Ds=Ds, val=[[0, 0], [0, 1]])
+    Z = yast.Tensor(config=config_dense, s=s)
+    Z.set_block(Ds=Ds, val=[[1, 0], [0, -1]])
+    EE = yast.Tensor(config=config_dense, s=s)
+    EE.set_block(Ds=Ds, val=[[1, 0], [0, 1]])
+
+    B = np.diag([mu]*chain)+np.diag([t]*(chain-1),1)+np.diag([t]*(chain-1),-1)
+    from_it, to_it = B.nonzero()
+    amplitude = B[B.nonzero()]
+    L = len(from_it)
+
+    permute_amp = [-1]*L
+    Tensor_from, Tensor_to = [None]*L, [None]*L
+    Tensor_conn, Tensor_other = [None]*L, [None]*L
+
+    for n in range(L):
+        if from_it[n]==to_it[n]:
+            Tensor_other[n] = EE
+            Tensor_from[n] = NN
+        else:
+            Tensor_other[n] = EE
+            Tensor_conn[n] = Z
+            Tensor_from[n] = CP
+            Tensor_to[n] = C
+    N, nr_phys, common_legs = chain, 2, (0, 1,)
+    return yamps.automatic_Mps(amplitude, from_it, to_it, permute_amp, Tensor_from, Tensor_to, Tensor_conn, Tensor_other, N, nr_phys, common_legs, opts={'tol': 1e-14})
