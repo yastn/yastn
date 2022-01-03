fe9f0d2533f3977f3a9418325226c145c465c0a5
marekrams <marek.rams@uj.edu.pl>
2022-01-03 13:50:14 +0100

    swap_gate tests

--- tests/tensor/configs/config_U1xU1_fermionic.py
+++ tests/tensor/configs/config_U1xU1_fermionic.py
@@ -1,5 +1,5 @@
 import yast.backend.backend_np as backend
-import yast.sym.sym_U1 as sym
+import yast.sym.sym_U1xU1 as sym
 fermionic = (True, True)
 default_device = 'cpu'
 default_dtype = 'float64'
