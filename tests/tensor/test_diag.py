3c4621e79488b716c1839036eb52d88cae2884d1
marekrams <marek.rams@uj.edu.pl>
2022-01-07 21:57:46 +0100

    diag

--- tests/tensor/test_diag.py
+++ tests/tensor/test_diag.py
@@ -1,3 +1,5 @@
+""" yast.diag() """
+import pytest
 import numpy as np
 import yast
 try:
@@ -8,7 +10,7 @@ except ImportError:
 tol = 1e-12  #pylint: disable=invalid-name
 
 
-def test_diag_1():
+def test_diag_basic():
     a1 = yast.rand(config=config_U1, s=(-1, 1),
                    t=((-1, 1, 2), (-1, 1, 2)),
                    D=((4, 5, 6), (4, 5, 6)))
@@ -29,8 +31,32 @@ def test_diag_1():
     assert yast.norm(a2 - a4) < tol  # == 0.0
     assert yast.norm(a3 - a5) < tol  # == 0.0
     assert yast.norm(a1 - a5) > tol  # are not identical
-    
+
+
+def test_diag_exceptions():
+    t1, D1, D2 = (-1, 0, 1), (2, 3, 4), (3, 4, 5)
+    with pytest.raises(yast.YastError):
+        a = yast.rand(config=config_U1, s=(-1, 1, 1), t=(t1, t1, t1), D=(D1, D1, D1))
+        _ = a.diag()  # Diagonal tensor requires 2 legs with opposite signatures.
+    with pytest.raises(yast.YastError):
+        a = yast.rand(config=config_U1, s=(1, 1), t=(t1, t1), D=(D1, D1))
+        _ = a.diag()  # Diagonal tensor requires 2 legs with opposite signatures.
+    with pytest.raises(yast.YastError):
+        a = yast.rand(config=config_U1, s=(-1, 1), t=(t1, t1), D=(D1, D1), n=1)
+        a.diag()  # Diagonal tensor requires zero tensor charge.
+    with pytest.raises(yast.YastError):
+        a = yast.rand(config=config_U1, s=(-1, 1, 1), t=(t1, t1, t1), D=(D1, D1, D1))
+        a.fuse_legs(axes=((0, 1), 2), mode='hard', inplace=True)
+        a.diag()  # Diagonal tensor cannot have fused legs.
+    with pytest.raises(yast.YastError):
+        a = yast.rand(config=config_U1, s=(-1, 1), t=(t1, t1), D=(D1, D1))
+        a.fuse_legs(axes=((0, 1),), mode='meta', inplace=True)
+        a.diag()  # Diagonal tensor cannot have fused legs.
+    with pytest.raises(yast.YastError):
+        a = yast.rand(config=config_U1, s=(-1, 1), t=(t1, t1), D=(D1, D2))
+        a.diag()  # yast.diag() allowed only for square blocks.
 
 
 if __name__ == '__main__':
-    test_diag_1()
+    test_diag_basic()
+    test_diag_exceptions()
