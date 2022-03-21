23828b237c5ed2248b7a45046249f51a7c35c5f8
Juraj Hasik <juraj.hasik.sk@gmail.com>
2022-03-21 20:01:14 +0100

    wip: custom backwards unmerge to 1d

--- yast/backend/backend_torch.py
+++ yast/backend/backend_torch.py
@@ -268,7 +268,7 @@ class kernel_transpose(torch.autograd.Function):
         for slice_to, slice_from, D_from in meta_transpose:
             inv_axes= tuple(np.argsort(axes))
             newdata_b[slice(*slice_from)] = data_b[slice(*slice_to)]\
-                .view(tuple(np.array(D_from)[[axes]])).permute(inv_axes).ravel()
+                .view(tuple(np.array(D_from)[np.array(axes)])).permute(inv_axes).ravel()
         return newdata_b, None, None
 
 def rsqrt(data, cutoff=0):
@@ -623,7 +623,7 @@ class kernel_merge_to_1d(torch.autograd.Function):
             tmp_b= data_b[slice(*slice_source)].view(D_source)
             for (_, slice_destination, D_destination, slice_source_block, D_source_block) in gr:
                 newdata_b[slice(*slice_destination)]= tmp_b[tuple(slice(*x) for x in slice_source_block)]\
-                    .reshape( tuple(np.array(D_destination)[[order]]) ).permute(inv_order).ravel()
+                    .reshape( tuple(np.array(D_destination)[np.array(order)]) ).permute(inv_order).ravel()
         return newdata_b, None, None, None, None
 
 
@@ -665,23 +665,40 @@ def unmerge_from_2ddiag(A, meta, new_sl, Dsize):
 
 
 def unmerge_from_1d(data, meta, new_sl, Dsize):
-    # slo -> slice in source tensor, specifying location of t_effective(fused) block
-    # Do  -> shape of the fused block with t_eff
-    newdata = torch.zeros((Dsize,), dtype=data.dtype, device=data.device)
-    for (slo, Do, sub_slc), snew in zip(meta, new_sl):
-        #                                                     take a "subblock" of t_eff block
-        #                                                     specified by a list of slices sub_slc
-        newdata[slice(*snew)] = data[slice(*slo)].reshape(Do)[tuple(slice(*x) for x in sub_slc)].ravel()
-    return newdata
+    return kernel_unmerge_from_1d.apply(data, meta, new_sl, Dsize)
 
+class kernel_unmerge_from_1d(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, data, meta, new_sl, Dsize):
+        ctx.meta= meta
+        ctx.new_sl= new_sl
+        ctx.fwd_data_size= data.size()
+
+        # slo -> slice in source tensor, specifying location of t_effective(fused) block
+        # Do  -> shape of the fused block with t_eff
+        newdata = torch.zeros((Dsize,), dtype=data.dtype, device=data.device)
+        for (slo, Do, sub_slc), snew in zip(meta, new_sl):
+            #                                                     take a "subblock" of t_eff block
+            #                                                     specified by a list of slices sub_slc
+            newdata[slice(*snew)] = data[slice(*slo)].reshape(Do)[tuple(slice(*x) for x in sub_slc)].ravel()
+        return newdata
+
+    @staticmethod
+    def backward(ctx, data_b):
+        meta= ctx.meta
+        new_sl= ctx.new_sl
+        fwd_data_size= ctx.fwd_data_size
+
+        newdata_b = torch.zeros(fwd_data_size, dtype=data_b.dtype, device=data_b.device)
+        for (slice_destination, D_eff_block, sub_slc), slice_source in zip(meta, new_sl):
+            newdata_b[slice(*slice_destination)].view(D_eff_block)[tuple(slice(*x) for x in sub_slc)]=\
+                data_b[slice(*slice_source)].view( tuple(x[1]-x[0] for x in sub_slc) )
+
+        return newdata_b, None, None, None
 
 #############
 #   tests   #
 #############
-
-
-
-
 
 def is_independent(x, y):
     """
