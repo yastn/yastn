e7c4fc6fa4960968f3d6002027ab8d6c8faf0452
marekrams <marek.rams@uj.edu.pl>
2022-05-18 16:28:08 +0200

    einsum

--- yast/tensor/_legs.py
+++ yast/tensor/_legs.py
@@ -5,7 +5,7 @@ __all__ = ['Leg']
 
 
 class _Leg(NamedTuple):
-    s: int = 1  # leg signature equall 1 or -1
+    s: int = 1  # leg signature in (1, -1)
     t: tuple = ()  # leg charges
     D: tuple = ()  # and their dimensions
 
@@ -17,21 +17,18 @@ def Leg(**kwargs):
     """ Test input of Leg. """
     if 's' in kwargs and kwargs['s'] not in (-1, 1):
         raise YastError('Signature of Leg should be 1 or -1')
-    if 't' in kwargs or 'D' in kwargs:
-        if 't' not in kwargs or 'D' not in kwargs:
-            raise YastError('Charges t and their dimensions D do not match')
-        try: 
-            kwargs['t'] = tuple(kwargs['t'])
-        except TypeError:
-             kwargs['t'] = (kwargs['t'],)
-        try: 
-            kwargs['D'] = tuple(kwargs['D'])
-        except TypeError:
-             kwargs['D'] = (kwargs['D'],)
-        # if not all(all(isinstance(x, int) for x in kwargs[field]) for field in ('t', 'D')):
-        #     raise YastError('Charges and dimensions should be int.')
-        # if any(x <= 0 for x in kwargs['D']):
-        #     raise YastError('Dimensions should be positive.')
-        if len(kwargs['D']) != len(kwargs['t']):
-            raise YastError('Charges t and their dimensions D do not match')
+    if 't' not in kwargs:
+        kwargs['t'] = ()
+    if 'D' not in kwargs:
+        kwargs['D'] = ()
+    try:   # simplify syntax for a single charge with nsym == 1
+        kwargs['t'] = tuple(kwargs['t'])
+    except TypeError:
+        kwargs['t'] = (kwargs['t'],)
+    try: 
+        kwargs['D'] = tuple(kwargs['D'])
+    except TypeError:
+        kwargs['D'] = (kwargs['D'],)
+    if len(kwargs['D']) != len(kwargs['t']):
+        raise YastError('Charges t and their dimensions D do not match')
     return _Leg(**kwargs)
