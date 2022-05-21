c848ed266c1e5f93fdb61ca438c49a5d56a71560
marekrams <marek.rams@uj.edu.pl>
2022-05-21 18:03:45 +0200

    fix tests

--- tests/mps/test_addition.py
+++ tests/mps/test_addition.py
@@ -13,26 +13,27 @@ except ImportError:
 tol = 1e-6
 
 
-def test_full_addition():
+def check_add(psi0, psi1):
+    """ test yamps.add using overlaps"""
+    out1 = yamps.add(tens=[psi0, psi1], amp=[1., 2.], common_legs=(1,))
+    o1 = yamps.measure_overlap(out1, out1)
+    p0 = yamps.measure_overlap(psi0, psi0)
+    p1 = yamps.measure_overlap(psi1, psi1)
+    p01 = yamps.measure_overlap(psi0, psi1)
+    p10 = yamps.measure_overlap(psi1, psi0)
+    assert abs(o1 - p0 - 4 * p1 - 2 * p01 - 2 * p10) < tol
+
+
+def test_addition():
     """create two Mps-s and add them to each other"""
     psi0 = ops_dense.mps_random(N=8, Dmax=15, d=1)
     psi1 = ops_dense.mps_random(N=8, Dmax=19, d=1)
+    check_add(psi0, psi1)
 
-    out0 = yamps.apxb(a=psi0, b=psi1, common_legs=(1,), x=2.)
-    out1 = yamps.add(tens=[psi0, psi1], amp=[1., 2.], common_legs=(1,))
-    assert yamps.measure_overlap(out0, out1) > 1 - tol
-
-
-def test_Z2_addition():
-    """create two Mps-s and add them to each other"""
     psi0 = ops_Z2.mps_random(N=8, Dblock=8, total_parity=0)
     psi1 = ops_Z2.mps_random(N=8, Dblock=12, total_parity=0)
-
-    out0 = yamps.apxb(a=psi0, b=psi1, common_legs=(1,), x=2.)
-    out1 = yamps.add(tens=[psi0, psi1], amp=[1., 2.], common_legs=(1,))
-    assert yamps.measure_overlap(out0, out1) > 1 - tol
+    check_add(psi0, psi1)
 
 
 if __name__ == "__main__":
-    test_full_addition()
-    test_Z2_addition()
+    test_addition()
