b6817713e5ebcad627d5d1703396f78db476ee0c
marekrams <marek.rams@uj.edu.pl>
2022-05-23 08:29:41 +0200

    leg

--- tests/tensor/test_leg.py
+++ tests/tensor/test_leg.py
@@ -10,21 +10,21 @@ tol = 1e-12  #pylint: disable=invalid-name
 
 
 def test_leg():
-    leg = yast.Leg(s=1, t=(1, 0, 1), D=(2, 3, 4))
+    leg = yast.Leg(config_U1, s=1, t=(1, 0, 1), D=(2, 3, 4))
     legc = leg.conj()
     assert leg.s == -legc.s
 
     with pytest.raises(yast.YastError):
-        _ = yast.Leg(s=2, t=(), D=())
+        _ = yast.Leg(config=config_U1, s=2, t=(), D=())
         # Signature of Leg should be 1 or -1
     with pytest.raises(yast.YastError):
-        _ = yast.Leg(s=1, t=(1, 0))
+        _ = yast.Leg(config=config_U1, s=1, t=(1, 0))
         # Charges t and their dimensions D do not match
 
     a = yast.ones(config=config_U1, s=(-1, 1, 1, 1),
                   t=((-2, 0, 2), (0, 2), (-2, 0, 2), 0),
                   D=((1, 2, 3), (1, 2), (1, 2, 3), 1))
-    legs = [a.get_leg_structure2(n) for n in range(a.ndim)]
+    legs = [a.get_leg(n) for n in range(a.ndim)]
     print(legs)
     b = yast.rand2(config=config_U1, legs=legs)
     b.show_properties()
--- yast/tensor/_legs.py
+++ yast/tensor/_legs.py
@@ -1,34 +1,41 @@
+import numpy as np
 from typing import NamedTuple
+from ._auxliary import _flatten
 from ._tests import YastError
+from ..sym import sym_none
 
 __all__ = ['Leg']
 
 
 class _Leg(NamedTuple):
+    sym: any = sym_none
     s: int = 1  # leg signature in (1, -1)
     t: tuple = ()  # leg charges
     D: tuple = ()  # and their dimensions
+    fused: tuple = ()  # subspaces
 
     def conj(self):
+        """ switch leg signature """
         return self._replace(s=-self.s)
 
 
-def Leg(**kwargs):
-    """ Test input of Leg. """
-    if 's' in kwargs and kwargs['s'] not in (-1, 1):
+def Leg(config, s=1, t=(), D=(), **kwargs):
+    """ Check input to create a new _Leg. """
+
+    sym = config if 'SYM_ID' in config else config.sym
+    if s not in (-1, 1):
         raise YastError('Signature of Leg should be 1 or -1')
-    if 't' not in kwargs:
-        kwargs['t'] = ()
-    if 'D' not in kwargs:
-        kwargs['D'] = ()
-    try:   # simplify syntax for a single charge with nsym == 1
-        kwargs['t'] = tuple(kwargs['t'])
-    except TypeError:
-        kwargs['t'] = (kwargs['t'],)
-    try: 
-        kwargs['D'] = tuple(kwargs['D'])
-    except TypeError:
-        kwargs['D'] = (kwargs['D'],)
-    if len(kwargs['D']) != len(kwargs['t']):
-        raise YastError('Charges t and their dimensions D do not match')
-    return _Leg(**kwargs)
+
+    D = tuple(_flatten(D))
+    t = tuple(_flatten(t))
+    if not all(isinstance(x, int) and x > 0 for x in D):
+        raise YastError('D should be a tuple of positive ints')
+    if not all(isinstance(x, int) for x in t):
+        raise YastError('Charges should be ints')
+    if len(D) * sym.NSYM != len(t) or (sym.NSYM == 0 and len(D) != 1):
+        raise YastError('Number of provided charges and bond dimensions do not match sym.NSYM')
+    newt = tuple(tuple(x.flat) for x in sym.fuse(np.array(t).reshape((len(D), 1, sym.NSYM)), s, s))
+    oldt = tuple(tuple(x.flat) for x in np.array(t).reshape(len(D), sym.NSYM))
+    if oldt != newt:
+        raise YastError('Provided charges are outside of the natural range for symmetry')
+    return _Leg(sym=sym, s=s, t=newt, D=D)
