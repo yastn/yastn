379c851e7c6e9a46231354964ef7bc63387631a7
g.wojtowicz@doctoral.uj.edu.pl <g.wojtowicz@doctoral.uj.edu.pl>
2022-06-28 16:21:09 +0200

    overload + and * for tensor to tensor operations, move add and multiply to _mps to avoid cyclic imports

--- tests/mps/test_algebra.py
+++ tests/mps/test_algebra.py
@@ -23,19 +23,32 @@ def check_add(psi0, psi1):
     assert abs(o1 - p0 - 4 * p1 - 2 * p01 - 2 * p10) < tol
 
 
+def check_add_mul(psi0, psi1):
+    """ test __add__ and __mul__ by a number using overlaps"""
+    out1 = (1.0 * psi0) + (2.0 * psi1)
+    o1 = yamps.measure_overlap(out1, out1)
+    p0 = yamps.measure_overlap(psi0, psi0)
+    p1 = yamps.measure_overlap(psi1, psi1)
+    p01 = yamps.measure_overlap(psi0, psi1)
+    p10 = yamps.measure_overlap(psi1, psi0)
+    assert abs(o1 - p0 - 4 * p1 - 2 * p01 - 2 * p10) < tol
+
+
 def test_addition():
     """create two Mps-s and add them to each other"""
     psi0 = ops_dense.mps_random(N=8, Dmax=15, d=1)
     psi1 = ops_dense.mps_random(N=8, Dmax=19, d=1)
     check_add(psi0, psi1)
+    check_add_mul(psi0, psi1)
 
     psi0 = ops_Z2.mps_random(N=8, Dblock=8, total_parity=0)
     psi1 = ops_Z2.mps_random(N=8, Dblock=12, total_parity=0)
     check_add(psi0, psi1)
+    check_add_mul(psi0, psi1)
 
 
 def test_multiply():
-    """ Calculate ground state and checks yamps.multiply() and yamps.add() within eigen-condition."""
+    """ Calculate ground state and checks yamps.multiply() and __mul__()and yamps.add() within eigen-condition."""
     ops_U1.random_seed(seed=0)
     N = 7
     Dmax = 8
@@ -52,10 +65,16 @@ def test_multiply():
 
     Hpsi = yamps.multiply(H, psi)
     assert pytest.approx(yamps.measure_overlap(Hpsi, Hpsi).item(), rel=tol) == Eng ** 2
-
     p0 = yamps.add(Hpsi, psi, amplitudes=[1, -Eng])
     assert yamps.measure_overlap(p0, p0) < tol  # == 0.
+    p0 = yamps.add(Hpsi * -1, Eng * psi)
+    assert yamps.measure_overlap(p0, p0) < tol  # == 0.
 
+
+    Hpsi = H*psi
+    assert pytest.approx(yamps.measure_overlap(Hpsi, Hpsi).item(), rel=tol) == Eng ** 2
+    p0 = yamps.add(Hpsi, psi, amplitudes=[1, -Eng])
+    assert yamps.measure_overlap(p0, p0) < tol  # == 0.
     p0 = yamps.add(Hpsi * -1, Eng * psi)
     assert yamps.measure_overlap(p0, p0) < tol  # == 0.
 
