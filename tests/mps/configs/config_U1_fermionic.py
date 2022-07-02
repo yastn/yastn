cd7c8b0b330074270c4c53b352fee77696d910f7
g.wojtowicz@doctoral.uj.edu.pl <g.wojtowicz@doctoral.uj.edu.pl>
2022-07-02 02:16:39 +0200

    generate mpo using fermionic swaps

---
+++ tests/mps/configs/config_U1_fermionic.py
@@ -0,0 +1,7 @@
+import yast.backend.backend_np as backend
+import yast.sym.sym_Z2 as sym
+default_device = 'cpu'
+default_dtype = 'float64'
+fermionic = True
+default_fusion = 'hard'
+force_fusion = None
