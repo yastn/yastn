6588e9d794c3fc3832c07fb3c4972a1428cdbba2
marekrams <marek.rams@uj.edu.pl>
2022-10-14 21:16:48 +0200

    tests

--- tests/mps/test_copy.py
+++ tests/mps/test_copy.py
@@ -1,16 +1,7 @@
 """ basic procedures of single mps """
 import numpy as np
-try:
-    from . import generate_random, generate_by_hand
-    from .configs import config_dense, config_dense_fermionic
-    from .configs import config_U1, config_U1_fermionic
-    from .configs import config_Z2, config_Z2_fermionic
-except ImportError:
-    import generate_random, generate_by_hand
-    from configs import config_dense, config_dense_fermionic
-    from configs import config_U1, config_U1_fermionic
-    from configs import config_Z2, config_Z2_fermionic
-
+import yast
+import yamps
 
 
 tol = 1e-12
@@ -19,35 +10,24 @@ tol = 1e-12
 def check_copy(psi1, psi2):
     """ Test if two mps-s have the same tensors (values). """
     for n in psi1.sweep():
-        assert np.allclose(psi1.A[n].to_numpy(), psi2.A[n].to_numpy())
+        assert np.allclose(psi1[n].to_numpy(), psi2[n].to_numpy())
+    assert psi1.A is not psi2.A
+    assert psi1 is not psi2
 
 
-def test_full_copy():
+def test_copy():
     """ Initialize random mps of full tensors and checks copying. """
-    psi = generate_random.mps_random(config_dense, N=16, Dmax=15, d=2)
+    operators = yast.operators.Spin1(sym='Z3')
+    generate = yamps.Generator(N=16, operators=operators)
+
+    psi = generate.random_mps(n=(1,), D_total=16)
     phi = psi.copy()
     check_copy(psi, phi)
 
-    psi = generate_random.mps_random(config_dense, N=16, Dmax=19, d=[2, 3])
-    phi = psi.copy()
-    check_copy(psi, phi)
-
-    psi = generate_random.mpo_random(config_dense, N=16, Dmax=25, d=[2, 3], d_out=[2, 1])
-    phi = psi.copy()
-    check_copy(psi, phi)
-
-
-def test_Z2_copy():
-    """ Initialize random mps of full tensors and checks copying. """
-    psi = generate_random.mps_random(config_Z2, N=16, Dblock=25, total_parity=0)
-    phi = psi.copy()
-    check_copy(psi, phi)
-
-    psi = generate_random.mps_random(config_Z2, N=16, Dblock=25, total_parity=1)
-    phi = psi.copy()
+    psi = generate.random_mpo(D_total=16)
+    phi = psi.clone()
     check_copy(psi, phi)
 
 
 if __name__ == "__main__":
-    test_full_copy()
-    test_Z2_copy()
+    test_copy()
