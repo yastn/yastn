8ab862cba61c43c43e087dfb41121b48c910dcb3
marekrams <marek.rams@uj.edu.pl>
2022-11-01 06:49:48 +0100

    example

--- yast/tn/mps/__init__.py
+++ yast/tn/mps/__init__.py
@@ -5,4 +5,5 @@ from ._env import Env2, measure_overlap, Env3, measure_mpo
 from ._dmrg import dmrg_sweep_1site, dmrg_sweep_2site, dmrg
 from ._tdvp import tdvp_sweep_1site, tdvp_sweep_2site, tdvp
 from ._compression import variational_sweep_1site, multiply_svd
-from ._generate import Generator, random_dense_mps, random_dense_mpo
+from ._generate import random_dense_mps, random_dense_mpo
+from ._generate import Generator, generate_H1, Hterm, generate_mpo
