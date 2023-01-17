592335d080aba12e871a4f507c28f835dae0c6ba
G4BR13L4 <g.wojtowicz@doctoral.uj.edu.pl>
2023-01-16 22:04:04 -0500

    typos in docus

--- yast/tn/mps/_auxliary.py
+++ yast/tn/mps/_auxliary.py
@@ -31,7 +31,7 @@ def load_from_dict(config, in_dict):
     return out_Mps
 
 
-def load_from_hdf5(config, file, in_file_path):
+def load_from_hdf5(config, file, my_address):
     r"""
     Create MPS/MPO from HDF5 file.
 
@@ -43,18 +43,20 @@ def load_from_hdf5(config, file, in_file_path):
     file: File
         A 'pointer' to a file opened by a user
 
+    my_address: str
+        Name of a group in the file, where the Mps is saved, e.g., './state/'
+
     Returns
     -------
     yast.MpsMpo
-    """
-    
-    sym_id = file[in_file_path].get('sym/SYM_ID')[()]
-    nsym = file[in_file_path].get('sym/NSYM')[()]
+    """    
+    sym_id = file[my_address].get('sym/SYM_ID')[()]
+    nsym = file[my_address].get('sym/NSYM')[()]
     if not sym_id.decode('ascii') == config.sym.SYM_ID or not nsym == config.sym.NSYM:
         raise YampsError("config doesn't match the one for saved data")
-    nr_phys = int(file[in_file_path].get('nr_phys')[()])
-    N = len(file[in_file_path+'/A'].keys())
+    nr_phys = int(file[my_address].get('nr_phys')[()])
+    N = len(file[my_address+'/A'].keys())
     out_Mps = MpsMpo(N, nr_phys=nr_phys)
     for n in range(out_Mps.N):
-        out_Mps.A[n] = initialize.load_from_hdf5(config, file, in_file_path+'/A/'+str(n))
+        out_Mps.A[n] = initialize.load_from_hdf5(config, file, my_address+'/A/'+str(n))
     return out_Mps
