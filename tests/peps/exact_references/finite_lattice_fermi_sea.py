dffc8ce5049f491566bc946b86250a0c925376c3
aritra <aritrasinha98@egmail.com>
2023-02-15 09:33:45 +0100

    fixed the finite CTM projectors

--- tests/peps/exact_references/finite_lattice_fermi_sea.py
+++ tests/peps/exact_references/finite_lattice_fermi_sea.py
@@ -49,12 +49,12 @@ def correlator(x1, y1, x2, y2, Nx, Ny, t, mu, beta):
     return val
 
 
-Nx = 2
-Ny = 2
+Nx = 3
+Ny = 3
 t = 1
-beta = 0.1
+beta = 3
 mu = 0
-x1, y1, x2, y2 = 1, 0, 1, 1
+x1, y1, x2, y2 = 2, 0, 2, 1
 x11, y11, x21, y21 = 0, 1, 1, 1
 
 c1 = correlator(x1, y1, x2, y2, Nx, Ny, t, mu, beta)
