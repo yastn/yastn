b770b6b8b2cfa8b8bfe5e5ae394610d189f6ab97
aritra <aritrasinha98@egmail.com>
2023-08-29 07:53:42 +0200

    minor cleanup

--- yastn/tn/fpeps/__init__.py
+++ yastn/tn/fpeps/__init__.py
@@ -1,3 +1,3 @@
 from ._geometry import Lattice, Peps, Bond
 from ._doublePepsTensor import DoublePepsTensor
-from ._initialization_peps import initialize_peps_purification, initialize_post_sampling_spinful_sz_basis
+from ._initialization_peps import initialize_peps_purification
