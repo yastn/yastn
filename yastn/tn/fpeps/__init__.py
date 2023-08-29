36f5f085d5be4115fe88b30502ca97c7477692c3
aritra <aritrasinha98@egmail.com>
2023-08-29 15:26:08 +0200

    small changes

--- yastn/tn/fpeps/__init__.py
+++ yastn/tn/fpeps/__init__.py
@@ -1,3 +1,3 @@
 from ._geometry import Lattice, Peps, Bond
 from ._doublePepsTensor import DoublePepsTensor
-from ._initialization_peps import initialize_peps_purification
+from ._initialization_peps import initialize_peps_purification, initialize_diagonal_basis
