1391319c9d47c18db2d441e0ffd219a808d03bb9
marekrams <marek.rams@uj.edu.pl>
2023-09-11 21:51:09 +0200

    new generate mpo

--- tests/mps/test_generator.py
+++ tests/mps/test_generator.py
@@ -150,7 +150,7 @@ def mpo_hopping_Hterm(N, J, sym="U1", config=None):
     # Generate MPO for Hterms
     #
     H = mps.generate_mpo(I, Hterms, opts={'tol':1e-14})
-    H2, k2 = mps.generate_mpo2(I, Hterms, opts={'tol':1e-14})
+    H2 = mps.generate_mpo2(I, Hterms, opts={'tol':1e-14})
     return H
 
 
