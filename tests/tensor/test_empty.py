1bf3968d3c92d299b918e906a89628a6fed05e27
marekrams <marek.rams@uj.edu.pl>
2023-10-06 21:13:10 +0200

    test_empty

---
+++ tests/tensor/test_empty.py
@@ -0,0 +1,28 @@
+""" Test tensor operations on an empty tensor """
+import pytest
+import numpy as np
+import yastn
+try:
+    from .configs import config_U1
+except ImportError:
+    from configs import config_U1
+
+tol = 1e-12  #pylint: disable=invalid-name
+
+
+def test_empty_tensor():
+    """ Test some tensor operations on an empty tensor. """
+    a = yastn.Tensor(config=config_U1, s=(1, 1, -1, -1))
+
+    assert a.norm() < tol
+
+    b = a.fuse_legs(axes=((0, 1), (2, 3)), mode='hard')
+    c = b.unfuse_legs(axes=(0, 1))
+    assert (a - c).norm() < tol
+
+    d = yastn.tensordot(a, a, axes=((2, 3), (0, 1)))
+    assert (a - d).norm() < tol
+
+
+if __name__ == '__main__':
+    test_empty_tensor()
