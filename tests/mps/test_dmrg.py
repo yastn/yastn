d9f9ab01b198188d1f4faf781e80d8896e2b0817
marekrams <marek.rams@uj.edu.pl>
2024-03-17 17:46:45 +0100

    fix dmrg test

--- tests/mps/test_dmrg.py
+++ tests/mps/test_dmrg.py
@@ -266,7 +266,7 @@ def test_dmrg_XX_model_U1_sum_of_Mpos(config=cfg, tol=1e-6):
         run_dmrg(psi, H, O_occ, E_target, occ_target, opts_svd, tol)
 
 
-def test_dmrg_Ising_PBC_Z2(config=cfg, tol=1e-5):
+def test_dmrg_Ising_PBC_Z2(config=cfg, tol=5e-5):
     """
     Initialize random MPS of U(1) tensors and tests _dmrg vs known results.
     """
@@ -298,7 +298,7 @@ def test_dmrg_Ising_PBC_Z2(config=cfg, tol=1e-5):
         0: [-10.2516617910, -8.6909392148, -7.2490195708, -7.2490195708, -7.2490195708, -7.2490195708, -6.1454220537],
         1: [-10.0546789842, -8.5239452547, -8.5239452547, -7.2262518595, -7.2262518595, -6.9932115253, -6.3591608542]}
 
-    Dmax = 16
+    Dmax = 20
     opts_svd = {'tol': 1e-10, 'D_total': Dmax}
 
     for H, P in [(H0, P0), (H1, P1)]:
@@ -308,8 +308,8 @@ def test_dmrg_Ising_PBC_Z2(config=cfg, tol=1e-5):
             psis = run_dmrg(psi, H, P, E_target, parity_target, opts_svd, tol)
             for EE, psi in zip(E_target, psis):
                 psi1 = mps.zipper(H, psi, opts_svd=opts_svd, normalize=False)
-                mps.compression_(psi1, [H, psi], method='2site', max_sweeps=2, opts_svd=opts_svd, normalize=False)
-                mps.compression_(psi1, [H, psi], method='1site', max_sweeps=6, normalize=False)
+                mps.compression_(psi1, [H, psi], method='2site', max_sweeps=4, opts_svd=opts_svd, normalize=False)
+                mps.compression_(psi1, [H, psi], method='1site', max_sweeps=10, normalize=False)
                 assert (EE * psi - psi1).norm() < tol
                 #
                 # evolve in real time  # test Heff0 in PBC
