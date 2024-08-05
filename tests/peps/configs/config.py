b8d961cf178d955e8119d4f4cef2568078a65bc9
marekrams <marek.rams@uj.edu.pl>
2024-08-05 13:04:15 +0200

    np in peps config

--- tests/peps/configs/config.py
+++ tests/peps/configs/config.py
@@ -1,5 +1,5 @@
 """ example of config; here used by pytest to modify and inject backend and device. """
-import yastn.backend.backend_torch as backend
+import yastn.backend.backend_np as backend
 import yastn.sym.sym_none as sym
 default_device = 'cpu'
 default_dtype = 'float64'
