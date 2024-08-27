6ef8ddde517c8b3b02649355f1554254bb6018a9
marekrams <marek.rams@uj.edu.pl>
2024-08-27 15:23:46 +0200

    bench dmrg

--- benchmarks/bench_dmrg.py
+++ benchmarks/bench_dmrg.py
@@ -12,43 +12,71 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-""" benchmark dmrg in Heisenberg model """
-import numpy as np
+""" benchmark DMRG in Heisenberg spin-1 model """
+import argparse
 import time
 import yastn.tn.mps as mps
 import yastn
 
 
-
-def dmrg_Heisenberg(config=None, tol=1e-6):
+def dmrg_Heisenberg(args):
     """
-    Initialize random MPS of dense tensors and
-    runs a few sweeps of DMRG with the Hamiltonian of XX model.
+    Initialize MPS in a Neel state and run a few sweeps of
+    DMRG 2-site with the Heisenberg spin-1 Hamiltonian.
     """
     N = 100
-    J = 1
-    D = 16
-    ops = yastn.operators.Spin1(sym='U1')
+    if args.backend == 'np':
+        import yastn.backend.backend_np as backend
+    elif args.backend == 'torch':
+        import yastn.backend.backend_torch as backend
+    ops = yastn.operators.Spin1(sym=args.sym, backend=backend, default_device=args.device)
     #
     sp, sm, sz = ops.sp(), ops.sm(), ops.sz()
     #
+    # Hamiltonian MPO
+    #
     I = mps.product_mpo(ops.I(), N)
-    terms = [mps.Hterm(J, [n, n+1], [sz, sz]) for n in range(N - 1)]
-    terms += [mps.Hterm(J / 2, [n, n+1], [sp, sm]) for n in range(N - 1)]
-    terms += [mps.Hterm(J / 2, [n, n+1], [sm, sp]) for n in range(N - 1)]
+    terms = [mps.Hterm(1, [n, n+1], [sz, sz]) for n in range(N - 1)]
+    terms += [mps.Hterm(1 / 2, [n, n+1], [sp, sm]) for n in range(N - 1)]
+    terms += [mps.Hterm(1 / 2, [n, n+1], [sm, sp]) for n in range(N - 1)]
     H = mps.generate_mpo(I, terms)
     #
-    psi = mps.random_mps(I, n=(0,), D_total=D, dtype='float64')
+    # Initial product state
     #
-    keep_time = time.time()
-    opts_svd = {"D_total": D}
-    info = mps.dmrg_(psi, H, method='2site',
-                     Schmidt_tol=1e-5, max_sweeps=32,
-                     opts_svd=opts_svd)
-    wall_time = time.time() - keep_time
-    print(info)
-    print(f"DMRG time = {wall_time:0.2f} s.")
-    print(f"Energy = {info.energy:0.5f}")
+    psi = mps.product_mps([ops.vec_z(1), ops.vec_z(-1)], N)
+    #
+    # setting up DMRG parameters
+    #
+    Ds = [10, 32, 64, 128, 256, 384, 512, 768, 1024, 1536, 2048]
+    opts_svd = {"D_total": Ds[0]}
+    rep = 2  # we will make two sweeps per D in Ds
+    dmrg = mps.dmrg_(psi, H, method='2site', iterator_step=1, max_sweeps=rep * len(Ds), opts_svd=opts_svd)
+    #
+    # execute generator dmrg
+    #
+    ref_time = time.time()
+    ref_time_global = ref_time
+    for info in dmrg:
+        wall_time = time.time() - ref_time
+        print(f"Sweep={info.sweeps:02d}; Energy={info.energy:4.12f}; D={max(psi.get_bond_dimensions()):4d}; time={wall_time:3.1f}sec.")
+        ref_time = time.time()
+        if info.sweeps % rep == 0 and info.sweeps // rep < len(Ds):
+            opts_svd["D_total"] = Ds[info.sweeps // rep]  # update D used by DMRG
+
+        if ref_time - ref_time_global > args.max_seconds:
+            print(f"Maximal simulation time reached with {ref_time - ref_time_global:0.1f}s.")
+            break
+
+    print("Cache info")
+    for x in yastn.get_cache_info().items():
+        print(x)
 
 if __name__ == "__main__":
-    dmrg_Heisenberg()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-sym", type=str, default='U1', choices=['Z3', 'dense', 'U1'])
+    parser.add_argument("-backend", type=str, default='np', choices=['np', 'torch']) # Verbose mode
+    parser.add_argument("-device", type=str, default='cpu', choices=['cpu', 'cuda']) # Verbose mode
+    parser.add_argument("-max_seconds", type=int, default=36) # Verbose mode
+    args = parser.parse_args()
+
+    dmrg_Heisenberg(args)
