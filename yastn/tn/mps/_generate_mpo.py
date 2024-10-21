026f4270b3224bc0075ab35ce897c1e7bbb17b42
marekrams <marek.rams@uj.edu.pl>
2024-10-21 10:13:34 +0200

    generate_mpo improve reshape of amplitudes for efficiency

--- yastn/tn/mps/_generate_mpo.py
+++ yastn/tn/mps/_generate_mpo.py
@@ -41,7 +41,7 @@ class Hterm(NamedTuple):
     operators: Sequence[yastn.Tensor]
         local operators in the product that are different than the identity.
         *i*-th operator is acting at ``positions[i]``.
-        Each operator should have ``ndim=2`` and signature ``s=(+1, -1)``.
+        Each operator should have ``ndim=2`` and signature matching local identity.
     """
     amplitude: float = 1.0
     positions: tuple = ()
@@ -50,7 +50,7 @@ class Hterm(NamedTuple):
 
 def ind_list_tensors(op: Tensor, unique: list) -> int:
     """ Return index of op in unique. If not present, append op to unique. """
-    ind = next((ind for ind, v in enumerate(unique) if v is op or allclose(v, op)), None)
+    ind = next((ind for ind, v in enumerate(unique) if v is op or (v.n == op.n and (v - op).norm() < 1e-13)), None)
     if ind is None:
         ind = len(unique)
         unique.append(op)
@@ -71,11 +71,10 @@ def generate_mpo(I, terms=None, opts_svd=None) -> yastn.tn.mps.MpsMpoOBC:
     r"""
     Generate MPO provided a list of :class:`Hterm`\-s and identity MPO ``I``.
 
-    It employs :meth:`mps.generate_mpo_preprocessing<yastn.tn.mps.generate_mpo_preprocessing>`
-    and :meth:`mps.generate_mpo_fast<yastn.tn.mps.generate_mpo_fast>`,
-    but without bookkeeping the template obtained in the preprocessing step.
-    The latter would speed up the generation of MPOs for different amplitudes
-    in front of the same set of product operators.
+    Apply swap_gates to introduce fermionic degrees of freedom.
+    The fermionic order matches the order of sites in Mps.
+    With this respect, local operators specified in term.operators are applied starting with the last element,
+    i.e., from right to left.
 
     Parameters
     ----------
@@ -190,7 +189,7 @@ def generate_mpo(I, terms=None, opts_svd=None) -> yastn.tn.mps.MpsMpoOBC:
         raise YastnError("generate_mpo: provided terms do not all have the same total charge.")
     tleft = tleft.pop()
 
-    trans = []
+    reshapes = []
     for n in I.sweep():
         mapH0 = mapH[:, 0].tolist()
         mapH, rind, iind = np.unique(mapH[:, 1:], axis=0, return_index=True, return_inverse=True)
@@ -199,16 +198,19 @@ def generate_mpo(I, terms=None, opts_svd=None) -> yastn.tn.mps.MpsMpoOBC:
 
         i2bs = {t: {} for t in t2bs[n].values()}
         for ii, rr in enumerate(rind):
-            i2bs[t2bs[n][mapH0[rr]]][ii] = len(i2bs[t2bs[n][mapH0[rr]]])
+            tmp = i2bs[t2bs[n][mapH0[rr]]]
+            tmp[ii] = len(tmp)
 
         i1bs = {t: 0 for t in t1bs[n].values()}
         for ii, rr in enumerate(mapH0):
             i1bs[t1bs[n][rr]] += 1
 
-        leg1 = Leg(config, s=-1, t=list(i1bs.keys()), D=list(i1bs.values()))
+        # leg1 = Leg(config, s=-1, t=list(i1bs.keys()), D=list(i1bs.values()))
         leg2 = basis[n].get_legs(axes=0).conj()
         leg3 = Leg(config, s=1, t=list(i2bs.keys()), D=[len(x) for x in i2bs.values()])
-        tran = zeros(config=config, legs=[leg1, leg2, leg3])
+
+        reshape = []
+        #zeros(config=config, legs=[leg1, leg2, leg3])
 
         li = {x: -1 for x in t1bs[n].values()}
         for bl, br in zip(mapH0, iind):
@@ -218,8 +220,8 @@ def generate_mpo(I, terms=None, opts_svd=None) -> yastn.tn.mps.MpsMpoOBC:
             fi = ifbs[n][bl]
             rt = t2bs[n][bl]
             ri = i2bs[rt][br]
-            tran[lt + ft + rt][li[lt], fi, ri] += 1
-        trans.append(tran)
+            reshape.append(((lt, ft, rt), (li[lt], fi, ri)))
+        reshapes.append((leg2, leg3, sorted(reshape)))
 
     amplitudes = [term.amplitude * sign for term, sign in zip(terms, signs)]
     dtype = 'complex128' if any(isinstance(a, complex) for a in amplitudes) else config.default_dtype
@@ -231,7 +233,15 @@ def generate_mpo(I, terms=None, opts_svd=None) -> yastn.tn.mps.MpsMpoOBC:
 
     O = Mpo(N)
     for n in O.sweep():
-        nJ = J @ trans[n]
+        leg1 = J.get_legs(axes=0)
+        leg2, leg3, reshape = reshapes[n]
+        nJ = zeros(config=config, legs=[leg1, leg2, leg3], dtype=dtype)
+        for (lt, ft, rt), group in groupby(reshape, key=itemgetter(0)):
+            if lt in leg1.t:
+                Jblock = J[lt + lt]
+                nJblock = nJ[lt + ft + rt]
+                for _, (li, fi, ri) in group:
+                    nJblock[:, fi, ri] += Jblock[:, li]
         nJ = ncon([nJ, basis[n]], [[0, 1, -3], [1, -1, -2]])
         if n < O.last:
             nJ, S, V = svd_with_truncation(nJ, axes=((0, 1, 2), 3), sU=1, **opts_svd)
@@ -241,215 +251,7 @@ def generate_mpo(I, terms=None, opts_svd=None) -> yastn.tn.mps.MpsMpoOBC:
         O[n] = nJ.transpose(axes=(0, 1, 3, 2))
     return O
 
-
-# def generate_product_mpo_from_Hterm(I, term, amplitude=True) -> yastn.tn.mps.MpsMpoOBC:
-#     r"""
-#     Apply local operators specified by term in :class:`Hterm` to
-#     the list of identities `I`. Generate product MPO as a result.
-#     As an input, insted of MPO, we take the list of identities for simpler manipulations.
-
-#     MPO `I` is presumed to be an identity.
-#     Apply swap_gates to introduce fermionic degrees of freedom
-#     (fermionic order is the same as the order of sites in Mps).
-#     With this respect, local operators specified in term.operators are applied starting with the last element,
-#     i.e., from right to left.
-
-#     Parameters
-#     ----------
-#     term: yastn.tn.mps.Hterm
-
-#     amplitude: bool
-#         if True, includes term.amplitude in MPO.
-#     """
-#     try:
-#         if len(term.positions) != len(term.operators):
-#             raise YastnError("Hterm: numbers of positions and operators do not match. ")
-#     except TypeError:
-#         raise YastnError("Hterm: positions and operators should be provided as lists or tuples.")
-
-#     tmp = I.copy()
-#     for site, op in zip(term.positions[::-1], term.operators[::-1]):
-#         if site < 0 or site > len(I) or not isinstance(site, numbers.Integral):
-#             raise YastnError("position in Hterm should be in 0, 1, ..., N-1 ")
-#         if op.s != I[site].s:
-#             raise YastnError("operator in Hterm should be a matrix with signature matching I")
-
-#         tmp[site] = op @ tmp[site]
-#         charge = op.n
-#         for n in range(site):
-#             tmp[n] = tmp[n].swap_gate(axes=0, charge=charge)
-
-#     psi = Mpo(len(I))
-#     rt = op.config.sym.zero()
-#     for n, vec in zip(psi.sweep(to='first'), tmp[::-1]):
-#         vec = vec.add_leg(axis=1, s=1, t=rt)
-#         rt = vec.n
-#         psi[n] = vec.add_leg(axis=0, s=-1)
-
-#     if amplitude: psi[0] = term.amplitude * psi[0]
-
-#     return psi
-
-
-# class GenerateMpoTemplate(NamedTuple):
-#     config: NamedTuple = None
-#     basis: list = None
-#     trans: list = None
-#     tleft: list = None
-
-
-# def generate_mpo_preprocessing(I, terms=None) -> GenerateMpoTemplate | tuple[GenerateMpoTemplate, list[float]]:
-#     r"""
-#     Precompute an amplitude-independent template that is used
-#     to generate MPO with :meth:`mps.generate_mpo_fast<yastn.tn.mps.generate_mpo_fast>`
-
-#     Parameters
-#     ----------
-#     I: yastn.tn.mps.MpsMpoOBC
-#         identity MPO.
-
-#     terms: list of :class:`Hterm`
-#         product operators making up the MPO.
-#     """
-#     if terms is None or len(terms) == 0:
-#         return GenerateMpoTemplate(trans=I)
-#     I2 = [I[n].remove_leg(axis=0).remove_leg(axis=1) for n in I.sweep(to='last')]
-#     H1s = [generate_product_mpo_from_Hterm(I2, term, amplitude=False) for term in terms]
-#     cfg = H1s[0][0].config
-#     mapH = np.zeros((len(H1s), I.N), dtype=np.int64)
-
-#     basis, t1bs, t2bs, tfbs, ifbs = [], [], [], [], []
-#     for n in I.sweep():
-#         base = []
-#         for m, H1 in enumerate(H1s):
-#             # site-tensors for which yastn.allclose is True are assumed idential.
-#             ind = next((ind for ind, v in enumerate(base) if allclose(v, H1[n])), None)
-#             if ind is None:
-#                 ind = len(base)
-#                 base.append(H1[n])
-#             mapH[m, n] = ind
-
-#         t1bs.append([ten.get_legs(axes=0).t[0] for ten in base])
-#         t2bs.append([ten.get_legs(axes=2).t[0] for ten in base])
-#         base = [ten.fuse_legs(axes=((0, 2), 1, 3)).drop_leg_history() for ten in base]
-#         tfb = [ten.get_legs(axes=0).t[0] for ten in base]
-#         tfbs.append(tfb)
-#         ifbs.append([sum(x == y for x in tfb[:i]) for i, y in enumerate(tfb)])
-#         base = block(dict(enumerate(base)), common_legs=(1, 2)).drop_leg_history()
-#         basis.append(base)
-
-#     tleft = [t1bs[0][i] for i in mapH[:, 0].tolist()]
-
-#     trans = []
-#     for n in I.sweep():
-#         mapH0 = mapH[:, 0].tolist()
-#         mapH, rind, iind = np.unique(mapH[:, 1:], axis=0, return_index=True, return_inverse=True)
-#         iind = iind.ravel().tolist()
-#         rind = rind.ravel().tolist()
-
-#         i2bs = {t: {} for t in t2bs[n]}
-#         for ii, rr in enumerate(rind):
-#             i2bs[t2bs[n][mapH0[rr]]][ii] = len(i2bs[t2bs[n][mapH0[rr]]])
-
-#         i1bs = {t: 0 for t in t1bs[n]}
-#         for ii, rr in enumerate(mapH0):
-#             i1bs[t1bs[n][rr]] += 1
-
-#         leg1 = Leg(cfg, s=-1, t=list(i1bs.keys()), D=list(i1bs.values()))
-#         leg2 = basis[n].get_legs(axes=0).conj()
-#         leg3 = Leg(cfg, s=1, t=list(i2bs.keys()), D=[len(x) for x in i2bs.values()])
-#         tran = zeros(config=cfg, legs=[leg1, leg2, leg3])
-
-#         li = {x: -1 for x in t1bs[n]}
-#         for bl, br in zip(mapH0, iind):
-#             lt = t1bs[n][bl]
-#             li[lt] += 1
-#             ft = tfbs[n][bl]
-#             fi = ifbs[n][bl]
-#             rt = t2bs[n][bl]
-#             ri = i2bs[rt][br]
-#             tran[lt + ft + rt][li[lt], fi, ri] += 1
-#         trans.append(tran)
-
-#     return GenerateMpoTemplate(config=cfg, basis=basis, trans=trans, tleft=tleft)
-
-
-# def generate_mpo_fast(template, amplitudes, opts_svd=None) -> yastn.tn.mps.MpsMpoOBC:
-#     r"""
-#     Fast generation of MPOs representing `Sequence[Hterm]` that differ only in amplitudes.
-
-#     Preprocessing in :meth:`yastn.tn.mps.generate_mpo` might be slow.
-#     When only amplitudes in Hterms are changing, e.g., for time-dependent Hamiltonian,
-#     MPO generation can be significantly speeded up by precalculating and reusing amplitude-independent ``template``.
-#     The latter is done with :meth:`yastn.tn.mps.generate_mpo_preprocessing`.
-
-#     Parameters
-#     ----------
-#     template: NamedTuple
-#         Calculated with :meth:`mps.generate_mpo_preprocessing<yastn.tn.mps.generate_mpo_preprocessing>`.
-#     amplidutes: Sequence[numbers]
-#         List of amplitudes that would appear in :class:`Hterm`.
-#         The order of the list should match the order of `Sequence[Hterm]`
-#         supplemented to :meth:`mps.generate_mpo_preprocessing<yastn.tn.mps.generate_mpo_preprocessing>`.
-#     opts: dict
-#         Options passed to :meth:`yastn.linalg.svd_with_truncation`.
-#         The function employs SVD while compressing the MPO bond dimensions.
-#         Default ``None`` sets truncation ``tol`` close to the numerical precision,
-#         which typically results in lossless compression.
-#     """
-#     if len(amplitudes) == 0:
-#         return template.trans.copy()
-
-#     if opts_svd is None:
-#         opts_svd = {'tol': 1e-13}
-
-#     Js = {}
-#     for a, t in zip(amplitudes, template.tleft):
-#         if t in Js:
-#             Js[t].append(a)
-#         else:
-#             Js[t] = [a]
-
-#     dtype = 'complex128' if any(isinstance(a, complex) for a in amplitudes) else template.config.default_dtype
-#     J = Tensor(config=template.config, s=(-1, 1), dtype=dtype)
-#     for t, val in Js.items():
-#         J.set_block(ts=(t, t), Ds=(1, len(val)), val=val)
-
-#     M = Mpo(len(template.basis))
-#     for n in M.sweep():
-#         nJ = J @ template.trans[n]
-#         nJ = ncon([nJ, template.basis[n]], [[0, 1, -3], [1, -1, -2]])
-#         if n < M.last:
-#             nJ, S, V = svd_with_truncation(nJ, axes=((0, 1, 2), 3), sU=1, **opts_svd)
-#             nS = S.norm()
-#             nJ = nS * nJ
-#             J = (S / nS) @ V
-#         M[n] = nJ.transpose(axes=(0, 1, 3, 2))
-#     return M
-
-
-# def generate_mpo(I, terms=None, opts_svd=None) -> yastn.tn.mps.MpsMpoOBC:
-#     r"""
-#     Generate MPO provided a list of :class:`Hterm`\-s and identity MPO ``I``.
-
-#     It employs :meth:`mps.generate_mpo_preprocessing<yastn.tn.mps.generate_mpo_preprocessing>`
-#     and :meth:`mps.generate_mpo_fast<yastn.tn.mps.generate_mpo_fast>`,
-#     but without bookkeeping the template obtained in the preprocessing step.
-#     The latter would speed up the generation of MPOs for different amplitudes
-#     in front of the same set of product operators.
-
-#     Parameters
-#     ----------
-#     terms: Sequence[yastn.tn.mps.Hterm]
-#         Product operators making up MPO.
-#     I: yastn.tn.mps.MpsMpoOBC
-#         Identity MPO.
-#     opts: dict
-#         Options passed to :meth:`yastn.linalg.svd_with_truncation`.
-#         The function employs SVD while compressing the MPO bond dimensions.
-#         Default ``None`` sets truncation ``tol`` close to the numerical precision,
-#         which typically results in lossless compression.
-#     """
-#     template = generate_mpo_preprocessing(I, terms)
-#     amplitudes = [term.amplitude for term in terms]
-#     return generate_mpo_fast(template, amplitudes, opts_svd=opts_svd)
+    # reshapes = []
+    #     reshape = zeros(config=config, legs=[leg1, leg2, leg3])
+    #         reshape[lt + ft + rt][li[lt], fi, ri] += 1
+    #     nJ = J @ reshapes[n]
