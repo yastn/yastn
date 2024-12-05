6be9fa0af0b6332703d9c7fb4cc112fbf6230ce9
marekrams <marek.rams@uj.edu.pl>
2024-12-05 21:37:15 +0100

    versioning with setuptools-scm

--- yastn/__init__.py
+++ yastn/__init__.py
@@ -16,4 +16,9 @@ from .tensor import *
 from .initialize import *
 from .krylov import *
 from . import operators
-from ._version import __version__
+try:
+    from ._version import version as __version__
+    from ._version import version_tuple
+except ImportError:
+    __version__ = "unknown version"
+    version_tuple = (0, 0, "unknown version")
