3c49e732c48440cd71ac9bd310b9dc5a29298879
marekrams <marek.rams@uj.edu.pl>
2025-08-17 10:14:10 +0200

    update test_addition peps

--- tests/peps/test_addition.py
+++ tests/peps/test_addition.py
@@ -23,35 +23,35 @@ tol = 1e-12  #pylint: disable=invalid-name
 
 
 def test_add_peps(config_kwargs):
-    ops = yastn.operators.SpinlessFermions(sym='Z2', **config_kwargs)
+    ops = yastn.operators.Spin12(sym='dense', **config_kwargs)
 
     geometry = fpeps.SquareLattice(dims=(2, 3), boundary='obc')
-    vec = {0: ops.vec_n(val=0), 1: ops.vec_n(val=1)}
-    occ0 = {(0, 0): 0, (0, 1): 1, (0, 2): 1, (1, 0): 1, (1, 1): 0, (1, 2): 0}
-    occ1 = {(0, 0): 0, (0, 1): 0, (0, 2): 1, (1, 0): 0, (1, 1): 1, (1, 2): 1}
-
+    vec = {-1: ops.vec_x(val=-1), 1: ops.vec_x(val=1)}
+    occ0 = {(0, 0): -1, (0, 1):  1, (0, 2): 1, (1, 0):  1, (1, 1): -1, (1, 2): -1}
+    occ1 = {(0, 0): -1, (0, 1): -1, (0, 2): 1, (1, 0): -1, (1, 1):  1, (1, 2):  1}
+    #
     psi0 = fpeps.product_peps(geometry, {k: vec[v] for k, v in occ0.items()})
     psi1 = fpeps.product_peps(geometry, {k: vec[v] for k, v in occ1.items()})
     #
     psi = psi0 + psi1
     env = fpeps.EnvCTM(psi, init='rand')
     env.iterate_(opts_svd={"D_total": 2}, max_sweeps=5, corner_tol=1e-4)
-    evn = env.measure_1site(ops.n())
-    assert psi[0, 0].unfuse_legs(axes=(0, 1)).shape == (1, 1, 2, 2, 1)
+    evs = env.measure_1site(ops.x())
+    assert psi[0, 0].unfuse_legs(axes=(0, 1)).shape == (1, 1, 2, 2, 2)
     assert psi[1, 2].unfuse_legs(axes=(0, 1)).shape == (2, 2, 1, 1, 2)
-    assert all(abs(v.item() - (0.5 * occ0[k] + 0.5 * occ1[k])) < tol for k, v in evn.items())
+    assert all(abs(v.item() - (0.5 * occ0[k] + 0.5 * occ1[k])) < tol for k, v in evs.items())
     #
     psi = fpeps.add(psi0, psi1, amplitudes=(3, 4))
     env = fpeps.EnvCTM(psi, init='rand')
     env.iterate_(opts_svd={"D_total": 2}, max_sweeps=5, corner_tol=1e-4)
-    evn = env.measure_1site(ops.n())
-    assert all(abs(v.item() - (0.36 * occ0[k] + 0.64 * occ1[k])) < tol for k, v in evn.items())
+    evs = env.measure_1site(ops.x())
+    assert all(abs(v.item() - (0.36 * occ0[k] + 0.64 * occ1[k])) < tol for k, v in evs.items())
     #
     geometry2 = fpeps.SquareLattice(dims=(2, 3), boundary='infinite')
     psi2 = fpeps.product_peps(geometry2, {k: vec[v] for k, v in occ0.items()})
     psi3 = fpeps.product_peps(geometry2, {k: vec[v] for k, v in occ1.items()})
     psi = psi2 + psi3
-    assert psi[0, 0].unfuse_legs(axes=(0, 1)).shape == (2, 2, 2, 2, 1)
+    assert psi[0, 0].unfuse_legs(axes=(0, 1)).shape == (2, 2, 2, 2, 2)
     assert psi[1, 2].unfuse_legs(axes=(0, 1)).shape == (2, 2, 2, 2, 2)
     #
     with pytest.raises(yastn.YastnError,
@@ -71,20 +71,41 @@ def test_to_tensor(config_kwargs):
     #
     geometry = fpeps.SquareLattice(dims=(2, 3), boundary='obc')
     vec = {0: ops.vec_n(val=0), 1: ops.vec_n(val=1)}
-    occ = {(0, 0): 0, (0, 1): 1, (0, 2): 1, (1, 0): 1, (1, 1): 0, (1, 2): 0}
+    occ0 = {(0, 0): 0, (0, 1): 0, (0, 2): 1, (1, 0): 0, (1, 1): 1, (1, 2): 1}
+    occ1 = {(0, 0): 0, (0, 1): 1, (0, 2): 1, (1, 0): 1, (1, 1): 0, (1, 2): 0}
     #
-    psi = fpeps.product_peps(geometry, {k: vec[v] for k, v in occ.items()})
-    psi = psi.to_tensor()
+    psi0 = fpeps.product_peps(geometry, {k: vec[v] for k, v in occ0.items()})
+    gates = [fpeps.gates.Gate([((-1) ** occ0[(1, 0)]) * ops.cp().add_leg(axis=2, s=-1), ops.c().add_leg(axis=2, s=1)], [(1, 0), (1, 1)]),
+             fpeps.gates.Gate([((-1) ** occ0[(0, 1)]) * ops.cp().add_leg(axis=2, s=-1), ops.c().add_leg(axis=2, s=1)], [(0, 1), (0, 2)]),
+             fpeps.gates.Gate([((-1) ** occ0[(0, 2)]) * ops.cp().add_leg(axis=2, s=-1), ops.c().add_leg(axis=2, s=1)], [(0, 2), (1, 2)])]
+    # moving charges is done to retain original auxiliary legs for subsequent addition
+    # there are swap-gates with auxiliary charges (if there was originally charge 1 on left site)
+    # that is why there is "-" in the last gate to ofset it
+    #
+    psi1 = psi0.shallow_copy()
+    for gate in gates:
+        fpeps.apply_gate_(psi1, gate)
+    #
+    env = fpeps.EnvBP(psi1)
+    env.iterate_(max_sweeps=5, diff_tol=1e-12)
+    evs = env.measure_1site(ops.n())
+    assert all(abs(evs[k] - occ1[k]) < tol for k in evs)
+    #
+    psi = (psi0 + psi1).to_tensor()
+    # remove auxiliary legs
     for k in [11, 9, 7, 5, 3, 1]:
          psi = psi.remove_leg(axis=k)
     #
-    phi = mps.product_mps([vec[occ[k]] for k in geometry.sites()])
-    phi = phi.to_tensor()
+    phi0 = mps.product_mps([vec[occ0[k]] for k in geometry.sites()])
+    phi1 = mps.product_mps([vec[occ1[k]] for k in geometry.sites()])
+    phi = (phi0 + phi1).to_tensor()
+    #
     assert (psi - phi).norm() < tol
     #
     # for infinite system, raise an error
+    #
     geometry = fpeps.SquareLattice(dims=(2, 3), boundary='infinite')
-    psi = fpeps.product_peps(geometry, {k: vec[v] for k, v in occ.items()})
+    psi = fpeps.product_peps(geometry, {k: vec[v] for k, v in occ0.items()})
     with pytest.raises(yastn.YastnError,
                        match='to_tensor\(\) works only for a finite Peps.'):
         psi = psi.to_tensor()
