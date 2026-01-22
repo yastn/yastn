fix yastn_dtype to include reduced precision types

tests/tensor/test_initialization.py
@@ -119,6 +119,11 @@ def test_initialize_U1(config_kwargs):
     assert npa.shape == a1.get_shape() == (6, 3, 6, 1)
     assert a1.size == np.sum(npa != 0.)
     assert a1.is_consistent()
+    #
+    # dtypes
+    for dtype in ['float64', 'float32', 'complex128', 'complex64', 'bool']:
+        a1 = yastn.ones(config=config_U1, legs=legs, dtype=dtype)
+        assert a1.yastn_dtype == dtype
 
     # 0-dim tensor
     a = yastn.ones(config=config_U1)  # s=()  # t=(), D=()
@@ -191,7 +196,6 @@ def test_initialize_U1(config_kwargs):
 
     a4 = yastn.rand_like(a3)
     assert a4.struct == a3.struct
-
 
 
 def test_initialize_Z2xU1(config_kwargs):

yastn/backend/backend_np.py
@@ -24,8 +24,11 @@ import scipy.sparse.linalg
 # non-deterministic initialization of random number generator
 rng = {'rng': np.random.default_rng(None)}  # initialize random number generator
 BACKEND_ID = "np"
-DTYPE = {'float32': np.float32, 'float64': np.float64,
-         'complex64': np.complex64, 'complex128': np.complex128}
+DTYPE = {'float32': np.float32,
+         'float64': np.float64,
+         'complex64': np.complex64,
+         'complex128': np.complex128,
+         'bool': bool}
 
 
 def cuda_is_available():
@@ -34,6 +37,10 @@ def cuda_is_available():
 
 def get_dtype(t):
     return t.dtype
+
+
+def get_yastn_dtype(t):
+    return t.dtype.name
 
 
 def is_complex(x):

yastn/backend/backend_torch.py
@@ -29,8 +29,8 @@ from ._backend_torch_backwards import kernel_transpose, kernel_transpose_and_mer
 
 __all__= [
     'DTYPE', 'cuda_is_available',
-    'get_dtype', 'is_complex', 'get_device', 'random_seed', 'grad',
-    'detach', 'detach_', 'clone', 'copy', 'randint',
+    'get_dtype', 'get_yastn_dtype', 'is_complex', 'get_device', 'random_seed',
+    'grad', 'detach', 'detach_', 'clone', 'copy', 'randint',
     'to_numpy', 'get_shape', 'get_size', 'diag_create', 'diag_get', 'real',
     'imag', 'max_abs', 'maximum', 'norm_matrix', 'delete', 'insert',
     'expm', 'first_element', 'item', 'sum_elements', 'norm', 'entropy',
@@ -53,7 +53,8 @@ BACKEND_ID = "torch"
 DTYPE = {'float32': torch.float32,
          'float64': torch.float64,
          'complex64': torch.complex64,
-         'complex128': torch.complex128}
+         'complex128': torch.complex128,
+         'bool': torch.bool}
 
 
 def cuda_is_available():
@@ -62,6 +63,11 @@ def cuda_is_available():
 
 def get_dtype(t):
     return t.dtype
+
+
+def get_yastn_dtype(t):
+    dtypes = [k for k, v in DTYPE.items() if t.dtype == v]
+    return dtypes[0] if len(dtypes) == 1 else 'unknown'
 
 
 def is_complex(x):

yastn/tensor/__init__.py
@@ -306,8 +306,10 @@ class Tensor:
 
     @property
     def yastn_dtype(self) -> str:
-        """ Return :code:`'complex128'` if tensor data are complex else :code:`'float64'`. """
-        return 'complex128' if self.config.backend.is_complex(self._data) else 'float64'
+        """
+        Return string representing data dtype, e.g., ``'complex128'``, ``'float64'``, ``'complex64'``, ``'float32'``. ``'bool'``.
+        """
+        return self.config.backend.get_yastn_dtype(self._data)
 
     @property
     def data(self) -> 'numpy.array' | 'torch.tensor':